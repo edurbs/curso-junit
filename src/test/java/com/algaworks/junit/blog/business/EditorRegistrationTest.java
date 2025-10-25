928e4ec0fabfbcdc972c664ed3046efcfe50c1df
Eduardo Soares <edurbs@gmail.com>
2025-10-25 17:22:23 -0300

    add: use mockito junit to mock and inject

--- src/test/java/com/algaworks/junit/blog/business/EditorRegistrationTest.java
+++ src/test/java/com/algaworks/junit/blog/business/EditorRegistrationTest.java
@@ -6,9 +6,10 @@ import com.algaworks.junit.custom.HumanPhraseDisplayNameGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @DisplayNameGeneration(HumanPhraseDisplayNameGenerator.class)
+@ExtendWith(MockitoExtension.class)
 class EditorRegistrationTest {
 
     @Mock
@@ -29,11 +31,8 @@ class EditorRegistrationTest {
 
     Editor editor;
 
-    AutoCloseable closeable;
-
     @BeforeEach
     void init() {
-        closeable = MockitoAnnotations.openMocks(this);
         editor = new Editor(null, "Nome", "asd@asd.com", BigDecimal.TEN, true);
         when(editorStorage.save(editor))
                 .thenReturn(new Editor(76L, "Nome", "asd@asd.com", BigDecimal.TEN, true));
