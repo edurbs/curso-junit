e867c2ea052bb8ac485fecd149754c7494b07f58
Eduardo Soares <edurbs@gmail.com>
2025-10-25 10:05:40 -0300

    add: tests

--- src/main/java/com/algaworks/junit/util/AccountBank.java
+++ src/main/java/com/algaworks/junit/util/AccountBank.java
@@ -4,18 +4,22 @@ import java.math.BigDecimal;
 
 
 public class AccountBank {
+    public static final String VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE
+            = "Value cannot be null, zero or negative";
+    public static final String VALUE_CANNOT_BE_NULL_OR_NEGATIVE
+            = "Value cannot be null or negative";
     private BigDecimal balance;
 
     public AccountBank(BigDecimal balance) {
-        if (balance == null) {
-            throw new IllegalArgumentException("Balance cannot be null");
+        if (balance == null || balance.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException(VALUE_CANNOT_BE_NULL_OR_NEGATIVE);
         }
         this.balance = balance;
     }
 
     public void withdraw(BigDecimal value) {
         if(value==null || value.compareTo(BigDecimal.ZERO) <= 0 ){
-            throw new IllegalArgumentException("Value cannot be null, zero or negative");
+            throw new IllegalArgumentException(VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE);
         }
         BigDecimal newBalance = balance.subtract(value);
         if(newBalance.compareTo(BigDecimal.ZERO)<=0){
@@ -26,7 +30,7 @@ public class AccountBank {
 
     public void deposit(BigDecimal value) {
         if(value==null || value.compareTo(BigDecimal.ZERO)<= 0){
-            throw  new IllegalArgumentException("Value cannot be null, zero or negative");
+            throw  new IllegalArgumentException(VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE);
         }
         balance = balance.add(value);
     }
--- src/test/java/com/algaworks/junit/util/AccountBankTest.java
+++ src/test/java/com/algaworks/junit/util/AccountBankTest.java
@@ -1,117 +1,171 @@
 package com.algaworks.junit.util;
 
+import com.algaworks.junit.custom.HumanPhraseDisplayNameGenerator;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(HumanPhraseDisplayNameGenerator.class)
 class AccountBankTest {
 
-    public static final String VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE = "Value cannot be null, zero or negative";
+    public static final String VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE
+            = "Value cannot be null, zero or negative";
+    public static final String VALUE_CANNOT_BE_NULL_OR_NEGATIVE
+            = "Value cannot be null or negative";
+    public static final String INSUFFICIENT_BALANCE
+            = "Insufficient balance";
 
-    @Nested
-    class Create{
-        @Test
-        void givenBalanceNull_whenCreate_thenThrowException() {
-            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> getAccountBank(null));
-            assertEquals( "Balance cannot be null", illegalArgumentException.getMessage());
-        }
-
-        @Test
-        void givenBalanceNotNull_whenCreate_thenDoesNotThrowsException() {
-            assertDoesNotThrow(() -> getAccountBank(BigDecimal.ONE.negate()));
-            assertDoesNotThrow(() -> getAccountBank(BigDecimal.ZERO));
-            assertDoesNotThrow(() -> getAccountBank(BigDecimal.ONE));
-        }
-
-    }
-
-    @Nested
-    class Balance{
-
-        @Test
-        void givenBalance_whenCreate_thenSetBalance(){
-            AccountBank accountBank = getAccountBank();
-            assertEquals(BigDecimal.ZERO, accountBank.balance());
-        }
-
-        @Test
-        void givenAccount_whenGetBalance_thenReturnBalnace(){
-            AccountBank accountBank = getAccountBank(BigDecimal.TEN);
-            assertEquals(BigDecimal.TEN, accountBank.balance());
-        }
-
-
-    }
-
-    @Nested
-    class Withdraw{
-        @Test
-        void givenValueNull_whenWithdraw_thenThrowException() {
-            AccountBank accountBank = getAccountBank();
-            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> accountBank.withdraw(null));
-            assertEquals(VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE, illegalArgumentException.getMessage());
-        }
-
-        @Test
-        void givenValueZero_whenWithDraw_thenThrowException(){
-            AccountBank accountBank = getAccountBank();
-            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> accountBank.withdraw(BigDecimal.ZERO));
-            assertEquals(VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE, illegalArgumentException.getMessage());
-        }
-        @Test
-        void givenValueNegative_whenWithDraw_thenThrowException(){
-            AccountBank accountBank = getAccountBank();
-            BigDecimal negative = BigDecimal.ONE.negate();
-            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> accountBank.withdraw(negative));
-            assertEquals(VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE, illegalArgumentException.getMessage());
-        }
-
-        @Test
-        void givenValue_whenWithdraw_thenSubtractFromBalance(){
-            AccountBank accountBank = getAccountBank(BigDecimal.TEN);
-            accountBank.withdraw(BigDecimal.TWO);
-            BigDecimal balance = accountBank.balance();
-            assertEquals(BigDecimal.valueOf(8L), balance);
-        }
-
-        @Test
-        void givenValue_whenWithdrawWithoutBalance_thenThrowException(){
-            AccountBank accountBank = getAccountBank(BigDecimal.TWO);
-            RuntimeException exception = assertThrows(RuntimeException.class, () -> accountBank.withdraw(BigDecimal.TEN));
-            assertEquals("Insufficient balance", exception.getMessage());
-        }
-    }
-
-    @Nested
-    class Deposit{
-
-        @Test
-        void givenInvalidValue_whenDeposit_thenThrowException(){
-            BigDecimal negative = BigDecimal.ZERO.negate();
-            AccountBank accountBank = getAccountBank();
-            assertThrows(IllegalArgumentException.class, () -> accountBank.deposit(null));
-            assertThrows(IllegalArgumentException.class, () -> accountBank.deposit(BigDecimal.ZERO));
-            assertThrows(IllegalArgumentException.class, () -> accountBank.deposit(negative));
-        }
-
-        @Test
-        void givenValidValue_whenDeposit_thenAddBalance(){
-            AccountBank accountBank = getAccountBank(BigDecimal.ONE);
-            accountBank.deposit(BigDecimal.ONE);
-            assertEquals(BigDecimal.TWO, accountBank.balance());
-        }
-
-    }
-
-    private AccountBank getAccountBank() {
-        return new AccountBank(BigDecimal.ZERO);
-    }
-    private AccountBank getAccountBank(BigDecimal balance) {
+    AccountBank create(BigDecimal balance) {
         return new AccountBank(balance);
     }
 
+
+    @Nested
+    class GivenBalanceNegative {
+
+        @Nested
+        class WhenCreate {
+
+            @Test
+            void thenThrowException() {
+                BigDecimal negativeBalance = BigDecimal.TEN.negate();
+                Executable accountBank = () -> create(negativeBalance);
+                IllegalArgumentException illegalArgumentException
+                        = assertThrows(IllegalArgumentException.class, accountBank);
+                assertEquals(VALUE_CANNOT_BE_NULL_OR_NEGATIVE, illegalArgumentException.getMessage());
+            }
+
+        }
+
+    }
+
+    @Nested
+    class GivenBalanceZero {
+
+        BigDecimal zeroBalance = BigDecimal.ZERO;
+
+        AccountBank createWithBalanceZero() {
+            return new AccountBank(zeroBalance);
+        }
+
+        @Nested
+        class WhenCreate {
+
+            @Test
+            void thenDoesNotThrowException() {
+                assertDoesNotThrow(GivenBalanceZero.this::createWithBalanceZero);
+            }
+
+        }
+
+        @Nested
+        class WhenGetBalance{
+
+            @Test
+            void thenBalanceIsZero(){
+                var sut = createWithBalanceZero();
+                assertEquals(BigDecimal.ZERO, sut.balance());
+            }
+        }
+
+        @Nested
+        class WhenWithdraw{
+
+            @Test
+            void thenThrowsException(){
+                var sut = createWithBalanceZero();
+                BigDecimal oneCent = new BigDecimal("0.01");
+                RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> sut.withdraw(oneCent));
+                assertEquals(INSUFFICIENT_BALANCE, runtimeException.getMessage());
+            }
+        }
+
+        @Nested
+        class WhenDeposit{
+
+            @Test
+            void thenIncreaseBalance(){
+                var sut = createWithBalanceZero();
+                sut.deposit(BigDecimal.ONE);
+                assertEquals(BigDecimal.ONE, sut.balance());
+            }
+        }
+    }
+
+
+    @Nested
+    class GivenBalanceTen {
+
+        @Nested
+        class WhenCreate {
+
+            @Test
+            void thenDoesNotThrowException() {
+                assertDoesNotThrow(() -> create(BigDecimal.TEN));
+            }
+
+        }
+
+        @Nested
+        class WhenWithdrawOneHundred{
+
+            @Test
+            void thenThrowException(){
+                var sut = create(BigDecimal.TEN);
+                var oneHundred = BigDecimal.valueOf(100);
+                Executable withdraw = () -> sut.withdraw(oneHundred);
+                var exception = assertThrows(RuntimeException.class, withdraw);
+                assertEquals(INSUFFICIENT_BALANCE, exception.getMessage());
+            }
+        }
+
+    }
+
+    @Nested
+    class GivenNull{
+        BigDecimal oneHundred = new BigDecimal("100");
+
+        @Nested
+        class WhenCreate{
+
+            @Test
+            void thenThrowsException(){
+                Executable create = () -> create(null);
+                var exception = assertThrows(IllegalArgumentException.class, create);
+                assertEquals(VALUE_CANNOT_BE_NULL_OR_NEGATIVE, exception.getMessage());
+            }
+        }
+
+        @Nested
+        class WhenWithdraw{
+
+            @Test
+            void thenThrowsException(){
+                AccountBank accountBank = create(oneHundred);
+                Executable withdraw = () -> accountBank.withdraw(null);
+                var exception = assertThrows(IllegalArgumentException.class, withdraw);
+                assertEquals(VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE, exception.getMessage());
+            }
+        }
+
+        @Nested
+        class WhenDeposit{
+
+            @Test
+            void thenThrowsException(){
+                AccountBank accountBank = create(oneHundred);
+                Executable deposit = () -> accountBank.deposit(null);
+                var exception = assertThrows(IllegalArgumentException.class, deposit);
+                assertEquals(VALUE_CANNOT_BE_NULL_ZERO_OR_NEGATIVE, exception.getMessage());
+            }
+
+        }
+
+    }
 
 }
